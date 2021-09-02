ea72f4198c2e44f3fffc9f0da67861e3f2fabec9
Aaryan <aaryan14032006@gmail.com>
2021-09-02 22:48:25 +0530

    Updated

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  sticky: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
