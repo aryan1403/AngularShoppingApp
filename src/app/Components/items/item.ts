d2c32a8158dba71f230d2adb08336fef2470315e
Aaryan <aaryan14032006@gmail.com>
2021-09-02 19:13:42 +0530

    Created item

---
+++ src/app/Components/items/item.ts
@@ -0,0 +1,7 @@
+export class item {
+    id: string | undefined;
+    itemname: string | undefined;
+    itemdesc: string | undefined;
+    price: number | undefined;
+    imageurl: string | undefined;
+}
